2d3cd6f8a919ced82a48f7b7f40043e779bf85f1
TrungNEMO <trungnemo@gmail.com>
2021-09-28 15:54:55 +0700

    revise the startup with the templates and add bootstraps

--- blog/views.py
+++ blog/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html', {})
+    return render(request, 'blog/index.html', {})
+
+def about(request):
+    return render(request, 'blog/about.html', {})
