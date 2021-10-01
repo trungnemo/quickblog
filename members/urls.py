48db57d82e1df27fb6d351e80e8dd9dff79d7abe
TrungNEMO <trungnemo@gmail.com>
2021-10-01 10:02:00 +0700

    Chaptter 4 Login-Logout

--- members/urls.py
+++ members/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from django.urls  import path
 from .views import sign_up
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('sign_up/', sign_up, name = 'member-sign-up' ),
-] 
+    path('login/', auth_views.LoginView.as_view(template_name ='members/login.html'), name = 'member-login' ),
+    path('logout/', auth_views.LogoutView.as_view(template_name ='members/logout.html'), name = 'member-logout' ),
+]  
