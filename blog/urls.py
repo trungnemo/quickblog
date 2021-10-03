71c77d87be54659e300fca648e869b318c314293
TrungNEMO <trungnemo@gmail.com>
2021-10-03 11:47:51 +0700

    Make the login as the default page

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from django.urls.resolvers import URLPattern 
 from .views import index, about
 urlpatterns = [
-    path('', index, name = "index"),
+    #path('', index, name = "index"),
+    path('blog/', index, name = "index"),
     path('about', about, name = "about"),
 ]
--- members/urls.py
+++ members/urls.py
@@ -5,6 +5,7 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('sign_up/', sign_up, name = 'member-sign-up' ),
-    path('login/', auth_views.LoginView.as_view(template_name ='members/login.html'), name = 'member-login' ),
+    #path('login/', auth_views.LoginView.as_view(template_name ='members/login.html'), name = 'member-login' ),
+    path('', auth_views.LoginView.as_view(template_name ='members/login.html'), name = 'member-login' ),
     path('logout/', auth_views.LogoutView.as_view(template_name ='members/logout.html'), name = 'member-logout' ),
 ]  
